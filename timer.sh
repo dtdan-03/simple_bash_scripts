dd05bfeeb4992d732c2826485bb83f7ac590c830
dtdan03 <93221652+dtdan-03@users.noreply.github.com>
2024-10-20 13:20:06 +0200

    20241020: add beginning and ending messages
    Just added a beginning and an ending message to make it look a bit better
    
    Also changed the presetting to 30s instead of 60s

--- timer.sh
+++ timer.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 echo " "
-echo "Simple timer (in seconds). Just hit ctrl+c if you want to stop it."
-echo "Presetting: 60 seconds. you can change it easily inside of the script."
+echo "Simple timer (in seconds). Just hit ctrl+c if you want to stop it earlier."
+echo "Presetting: 30 seconds. you can change it easily inside of the script."
+echo " "
+echo "The countdown has started!"
 echo " "
 
-myvar=60 # seconds, you can change the time if you want to.
+myvar=30 # seconds, you can change the time if you want to.
 
-while [ $myvar -ne 0 ] # as long as we do not reach 0 seconds...
+while [ $myvar -ne 0 ] # "as long as we do not reach 0 seconds..."
 do
     echo $myvar
     myvar=$(( $myvar -1 )) 
     sleep 1
 done
+
+echo " "
+echo "The time is up!"
+echo " "
