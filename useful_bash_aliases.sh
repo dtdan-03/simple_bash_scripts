fdf3117e717737da1aeaad9ef086d76454ac35e6
Daniel <93221652+dtdan-03@users.noreply.github.com>
2025-11-09 16:40:13 +0100

    Go from dist-upgrade to full-upgrade & more
    apt dist-upgrade works, but it is only an alias for apt full-upgrade. dist-upgrade has been used as an combination with apt-get.
    
    Also:
    - added colored cat (thanks bat!)
    - more just for fun commands/aliases

--- useful_bash_aliases.sh
+++ useful_bash_aliases.sh
@@ -8,15 +8,29 @@
 alias gits='git status'
 alias gitp='git pull'
 
-alias urandom='cat /dev/urandom'
+# Get fast to Repositories
+alias Ansible='cd ~/src/Ansible/'
+alias github-dir='cd ~/src/ZZZ_GitHub'
 
 # Updates
 alias Update='sudo apt update' # Check for updates
-alias Upgrade='sudo apt dist-upgrade' # Install Updates using dist-upgrade
-alias Updates='sudo apt update && sudo apt dist-upgrade -y' # check for and install updates 
+alias Upgrade='sudo apt full-upgrade' # Install Updates using full-upgrade
+# Alternative: sudo apt dist-upgrade or sudo apt-get dist-upgrade (→ more information: man apt-get)
+alias Updates='sudo apt update && sudo apt full-upgrade -y' # check for and install updates 
 
+# More colors for your shell
+# Colorful cat output with the help of bat:
+# → sudo apt install bat
+alias cat='batcat --style=plain --paging=never --wrap=never'
+
+
+# Just for fun
+alias urandom='cat /dev/urandom'
+alias weather='curl wttr.in' # Just for fun. Outputs the weather report for your area.
+alias current-time='watch -n 1 date'
 alias uptime-counter='while true; do cat /proc/uptime | awk "{print int(\$1)}"; sleep 1; done' # real-time output of your uptime in seconds
-alias weather='curl wttr.in'
+# Alert notification:
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 # VirtualBox / VMs
 alias debian12vm_vm_start_without_open_vbox_vm_manager='VBoxManage startvm "debian12"' 
